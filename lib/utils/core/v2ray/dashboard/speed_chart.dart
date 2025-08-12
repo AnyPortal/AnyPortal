46d12a58dd89f6ec854f997616b60f23ce9acf2b
anyportal <anyportal.dev@gmail.com>
2025-08-12 08:57:08 +0000

    fix: SpeedChart: leftTitles: maxIncluded, reservedSize

--- lib/utils/core/v2ray/dashboard/speed_chart.dart
+++ lib/utils/core/v2ray/dashboard/speed_chart.dart
@@ -38,49 +38,43 @@ class _SpeedChartState extends State<SpeedChart> {
         return Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(
-              height: 12,
-            ),
-            Stack(children: [
-              AspectRatio(
-                aspectRatio: 3,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 24.0),
-                  child: LineChart(
-                    LineChartData(
+            const SizedBox(height: 12),
+            Stack(
+              children: [
+                AspectRatio(
+                  aspectRatio: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 24.0),
+                    child: LineChart(
+                      LineChartData(
                         minY: 0,
                         lineBarsData: dataNotifier.trafficQs
                             .map((t, q) {
                               return MapEntry(
-                                  t,
-                                  LineChartBarData(
-                                    spots: q,
-                                    dashArray:
-                                        t.name.contains("Up") ? [5, 10] : null,
-                                    dotData: const FlDotData(
-                                      show: false,
-                                    ),
-                                    isCurved: false,
-                                    color: trafficColor[t],
-                                  ));
+                                t,
+                                LineChartBarData(
+                                  spots: q,
+                                  dashArray: t.name.contains("Up")
+                                      ? [5, 10]
+                                      : null,
+                                  dotData: const FlDotData(show: false),
+                                  isCurved: false,
+                                  color: trafficColor[t],
+                                ),
+                              );
                             })
                             .values
                             .toList(),
                         titlesData: const FlTitlesData(
                           show: true,
                           bottomTitles: AxisTitles(
-                            sideTitles: SideTitles(
-                              showTitles: false,
-                            ),
+                            sideTitles: SideTitles(showTitles: false),
                           ),
                           leftTitles: AxisTitles(
                             drawBelowEverything: true,
                             sideTitles: SideTitles(
                               showTitles: true,
-                              reservedSize: 40,
-                              maxIncluded: false,
-                              // getTitlesWidget: (value, meta) => Text(
-                              //     formatBytes(value.toInt(), fractionDigits: 0)),
+                              reservedSize: 48,
                             ),
                           ),
                           rightTitles: AxisTitles(
@@ -90,51 +84,39 @@ class _SpeedChartState extends State<SpeedChart> {
                             sideTitles: SideTitles(showTitles: false),
                           ),
                         ),
-                        borderData: FlBorderData(
-                          show: false,
-                        )),
+                        borderData: FlBorderData(show: false),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Positioned(
-                left: 44,
-                top: 0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "${context.loc.proxy} ↑ ┄┄",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 10,
+                Positioned(
+                  left: 44,
+                  top: 0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "${context.loc.proxy} ↑ ┄┄",
+                        style: TextStyle(color: Colors.blue, fontSize: 10),
                       ),
-                    ),
-                    Text(
-                      "${context.loc.proxy} ↓ ──",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 10,
+                      Text(
+                        "${context.loc.proxy} ↓ ──",
+                        style: TextStyle(color: Colors.blue, fontSize: 10),
                       ),
-                    ),
-                    Text(
-                      "${context.loc.direct} ↑ ┄┄",
-                      style: TextStyle(
-                        color: Colors.orange,
-                        fontSize: 10,
+                      Text(
+                        "${context.loc.direct} ↑ ┄┄",
+                        style: TextStyle(color: Colors.orange, fontSize: 10),
                       ),
-                    ),
-                    Text(
-                      "${context.loc.direct} ↓ ──",
-                      style: TextStyle(
-                        color: Colors.orange,
-                        fontSize: 10,
+                      Text(
+                        "${context.loc.direct} ↓ ──",
+                        style: TextStyle(color: Colors.orange, fontSize: 10),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              )
-            ]),
+              ],
+            ),
           ],
         );
       },
